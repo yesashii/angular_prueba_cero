d28e1602ae1ab33c9787241ff31c4f064fd01f3a
Luis Herrera <lherrera@gignosoft.cl>
2017-06-07 17:01:33 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VerticalMenuComponent } from './vertical-menu/vertical-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VerticalMenuComponent
   ],
   imports: [
     BrowserModule
